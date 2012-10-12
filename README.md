86d09b81e61d711051efd14c55001da2306d4092
Luke Hudson <lukeletters@gmail.com>
2012-10-12 08:51:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FileSysCompletion #
+
+Enable auto-completion of paths from the file system à la VIM.
+
+## Licence ##
+
+Copyright Luke Hudson <lukeletters@gmail.com>, all parts of this 
+plugin are licensed under GPL v3 (see LICENSE.txt)
---
+++ fscompletion.py
@@ -0,0 +1,48 @@
+import sublime_plugin
+from glob import iglob
+import os
+import re
+
+# Enable SublimeText2 to complete filesystem paths a la VIM:
+# @author Luke Hudson <lukeletters@gmail.com>
+#
+# Test cases:
+#      /home/
+#      C:\abc
+#
+
+
+class FileSystemCompCommand(sublime_plugin.EventListener):
+    """
+    Enable SublimeText2 to complete filesystem paths a la VIM:
+    """
+
+    def on_query_completions(self, view, prefix, locations):
+
+        # Find the current location (of first selection)
+        rowcol = view.rowcol(locations[0])
+        line = view.line(locations[0])
+        # print "rowcol = %s" % str(rowcol)
+        # print "line = %s" % line
+
+        # Extract the whole line's text (prefix parameter above isn't enough)
+        lstr = view.substr(line)
+        lstr = lstr[0:rowcol[1] + 1]
+
+        # Now strip off leading comments or whitespaces.
+        lstr = re.sub(r'^[#/*]*[ \t]*', '', lstr)
+        if (lstr.find(r'\\') != -1):
+            lstr = re.sub(r'.*([a-z]:\\.*)', r'\1', lstr, flags=re.IGNORECASE)
+        elif (lstr.find('/') != -1):
+            lstr = re.sub(r'[^/]*(.*)', r'\1', lstr)
+        else:
+            return None
+        print "linetext = %s" % lstr
+
+        # Generate completions from the path found.
+        # We return tuples because this should be (label,completion)
+        matches = []
+        for x in iglob(lstr + "*"):
+            y = os.path.basename(x)
+            matches.append((y, y))
+        return matches
