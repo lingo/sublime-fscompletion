47c3f96d016629f71f9e3c3ef8f6d9621e76b01a
Luke Hudson <lukeletters@gmail.com>
2014-04-02 20:26:12 +0200

    USE add_slash setting

--- fscompletion.py
+++ fscompletion.py
@@ -153,7 +153,8 @@ class FileSystemCompCommand(sublime_plugin.EventListener):
 
             if os.path.isdir(fname):
                 text = '%s/\tDir' % completion
-                completion = completion + '/' # Append the slash for dirs
+                if (settings.get('add_slash', True)):
+                    completion = completion + '/' # Append the slash for dirs
             else:
                 text = '%s\tFile' % completion
 
